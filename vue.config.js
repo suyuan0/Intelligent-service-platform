1517467f357d028240c3381979b6228e0906c691
培培 <614963845@qq.com>
2022-07-11 16:11:37 +0800

    feat(proxy): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    open: true,
+    port: '8888',
+    host: 'localhost',
+    proxy: {
+      [process.env.VUE_APP_API]: {
+        target: process.env.VUE_APP_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_API]: ''
+        }
+      }
+    }
+  }
 })
